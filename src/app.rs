e89dbd9d81405576e7810b4fe6d590a5bfe71709
Jimmie <jimmie@neisep.com>
2026-04-10 16:00:07 +0200

    fix(opengl): issues with opening application on windows

--- src/app.rs
+++ src/app.rs
@@ -81,7 +81,7 @@ impl Drop for VlcChromecastApp {
 }
 
 impl eframe::App for VlcChromecastApp {
-    fn on_exit(&mut self, _gl: Option<&eframe::glow::Context>) {
+    fn on_exit(&mut self) {
         self.cleanup();
     }
 
